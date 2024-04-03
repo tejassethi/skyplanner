e3e7a59d51cb2126539d5fcd5ed5392273f55665
mastereater <mastereaterwebsite@gmail.com>
2024-04-04 00:10:46 +0700

    fix teaarea zoom

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,10 @@ import "../styles/global.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="lofi">
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+      />
       <body>{children}</body>
     </html>
   );
